037bf866cc928107527a0ca7a04f2004b2e256ca
bersling <bersling@gmail.com>
2017-03-05 12:57:06 +0100

    wip

---
+++ src/business-card-options.model.ts
@@ -0,0 +1,7 @@
+export interface BusinessCardOptions {
+  //content options
+  avatar?: string;
+
+  //layout options
+
+}
--- src/sample.component.ts
+++ src/sample.component.ts
@@ -1,12 +1,33 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {BusinessCardOptions} from "./business-card-options.model";
 
 @Component({
-  selector: 'sample-component',
-  template: `<h1>Sample component</h1>`
+  selector: 'business-card',
+  template: `
+<div class="business-card-wrapper">
+  <div class="business-card-header"></div>
+  <div class="business-card-avatar">
+    <img [src]="options.avatar"
+         alt="Avatar">
+  </div>
+  {{options}}
+  <div class="business-card-info">
+    <div class="business-card-name">
+      {{name}}
+    </div>
+  </div>
+</div>
+`
 })
 export class SampleComponent {
 
   constructor() {
   }
+
+  @Input()
+  name: string;
+
+  @Input()
+  options: BusinessCardOptions;
 
 }
